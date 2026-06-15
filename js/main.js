290bb093eb530b1a106374f2b906a9ef284a8a68
Gadiaga196 <gadiagandeyeabsa@gmail.com>
2026-06-15 06:32:13 +0000

    Finalisation : corrections responsive, optimisation du code et commentaires

--- js/main.js
+++ js/main.js
@@ -105,63 +105,66 @@ if(filtre){
 }
 // Affichage de l'erreur sur la page contact
 const form = document.getElementById("contactForm");
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const prenom = document.getElementById("prenom").value.trim();
-    const nom = document.getElementById("nom").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
-    const telephone = document.getElementById("telephone").value.trim();
-    const addresse = document.getElementById("addresse").value.trim();
-    let valide = true;
-    document.getElementById("prenomError").textContent = "";
-    document.getElementById("nomError").textContent = "";
-    document.getElementById("emailError").textContent = "";
-    document.getElementById("messageError").textContent = "";
-    document.getElementById("telephoneError").textContent = "";
-    document.getElementById("addresseError").textContent = "";
-    if(prenom === ""){
-        document.getElementById("prenomError").textContent = "Le prenom est obligatoire";
-        valide = false;
-    }
-    if(nom === ""){
-        document.getElementById("nomError").textContent = "Le nom est obligatoire";
-        valide = false;
-    }
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailRegex.test(email)){
-        document.getElementById("emailError").textContent = "Adresse email invalide";
-        valide = false;
-    }
-    if(message.length < 20){
-        document.getElementById("messageError").textContent = "Le message doit contenir au moins 20 caractères";
-        valide = false;
-    }
-    if(telephone === ""){
-        document.getElementById("telephoneError").textContent = "Le numero est invalide"; 
-        valide = false;
-    }
-    if(addresse === ""){
-        document.getElementById("addresseError").textContent = "L'addresse est invalide";
-        valide = false;
-    }
-    if(valide){
-        document.getElementById("successMessage").classList.remove("d-none");
-    }
-    if(valide){
-        document.getElementById("successMessage").classList.remove("d-none");
-        form.reset();
-    }
-    const cancelBtn = document.getElementById("cancelBtn");
-    cancelBtn.addEventListener("click", () => {
-        form.reset();
+if(form){
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const prenom = document.getElementById("prenom").value.trim();
+        const nom = document.getElementById("nom").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const message = document.getElementById("message").value.trim();
+        const telephone = document.getElementById("telephone").value.trim();
+        const addresse = document.getElementById("addresse").value.trim();
+        let valide = true;
         document.getElementById("prenomError").textContent = "";
-    document.getElementById("nomError").textContent = "";
-    document.getElementById("emailError").textContent = "";
-    document.getElementById("messageError").textContent = "";
-    document.getElementById("telephoneError").textContent = "";
-    document.getElementById("addresseError").textContent = "";
+        document.getElementById("nomError").textContent = "";
+        document.getElementById("emailError").textContent = "";
+        document.getElementById("messageError").textContent = "";
+        document.getElementById("telephoneError").textContent = "";
+        document.getElementById("addresseError").textContent = "";
+        if(prenom === ""){
+            document.getElementById("prenomError").textContent = "Le prenom est obligatoire";
+            valide = false;
+         }
+        if(nom === ""){
+            document.getElementById("nomError").textContent = "Le nom est obligatoire";
+            valide = false;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            document.getElementById("emailError").textContent = "Adresse email invalide";
+            valide = false;
+        }
+        if(message.length < 20){
+            document.getElementById("messageError").textContent = "Le message doit contenir au moins 20 caractères";
+            valide = false;
+        }
+        if(telephone === ""){
+            document.getElementById("telephoneError").textContent = "Le numero est invalide"; 
+            valide = false;
+        }
+        if(addresse === ""){
+            document.getElementById("addresseError").textContent = "L'addresse est invalide";
+            valide = false;
+        }
+        if(valide){
+            document.getElementById("successMessage").classList.remove("d-none");
+        }
+        if(valide){
+            document.getElementById("successMessage").classList.remove("d-none");
+            form.reset();
+        }
+        //Boutton d'annulation
+        const cancelBtn = document.getElementById("cancelBtn");
+        cancelBtn.addEventListener("click", () => {
+            form.reset();
+            document.getElementById("prenomError").textContent = "";
+            document.getElementById("nomError").textContent = "";
+            document.getElementById("emailError").textContent = "";
+            document.getElementById("messageError").textContent = "";
+            document.getElementById("telephoneError").textContent = "";
+            document.getElementById("addresseError").textContent = "";
 
-    document.getElementById("successMessage").classList.add("d-none");
+            document.getElementById("successMessage").classList.add("d-none");
+        });
     });
-});
+}
